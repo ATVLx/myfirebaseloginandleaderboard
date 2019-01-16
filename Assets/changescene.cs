ef94378f3744549077d1da88d6f5c642639a8b9c
Vishal-iMac <vlxmenblack@gmail.com>
2019-01-16 16:25:12 +0530

    videochanges from mac

---
+++ Assets/changescene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changescene : MonoBehaviour {
+    private void Awake()
+    {
+        StartCoroutine(changesceneinintro());
+    }
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    IEnumerator changesceneinintro(){
+        yield return new WaitForSecondsRealtime(16f);
+        SceneManager.LoadScene("Login");
+    }
+}
